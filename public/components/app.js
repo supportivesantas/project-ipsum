1833d9754cde30363684574bf8f2f01a7d641494
Matt Bresnan <bresnan.mw@gmail.com>
2016-04-26 17:29:06 -0700

    Init

---
+++ public/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+// Prefered way for stateless components:
+function App() {
+  return (
+    <div>
+      SUPER COOL AWESOME MEGA FANTASTIC INCREDIBLE APP!!!!!!!
+    </div>
+  );
+}
+
+export default App;
