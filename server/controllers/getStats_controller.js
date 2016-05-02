00ff1a6f7484d77571eae25645a2de4e145759fe
Rane Gridley <ranegri3@gmail.com>
2016-05-02 15:23:33 -0700

    Add functionality to get total hits for a server

--- server/controllers/getStats_controller.js
+++ server/controllers/getStats_controller.js
@@ -13,18 +13,25 @@ exports.singleServer = function(req, res) {
   // }
 
   //TODO: narrow down search for only entries from last ~6hours/// OR for summary information/ day, week, month, etc;
-  //TODO: Add 'total' obj to front of array, total hits for all
   stats.model.where('clientServers_id', serverId).fetchAll()
     .then(function(serverStats) {
+      var totalData = {
+        route: 'Total',
+        data: [],
+      };
+      var timesForTotal = {};
+      var totalCounter = 0;
       var count = 0;
       var routes = {};
       var graphData = [];
       serverStats.models.forEach(function(model) {
         var routeDataPoint = model.attributes;
+        var timestamp = routeDataPoint.created_at
+        // .slice(0, 19);
 
         var routeData = {
           hits: routeDataPoint.statValue,
-          time: routeDataPoint.created_at
+          time: timestamp,
         };
 
         if (routeDataPoint.statName in routes) {
@@ -33,12 +40,20 @@ exports.singleServer = function(req, res) {
           routes[routeDataPoint.statName] = count;
           graphData[count] = {
             route: routeDataPoint.statName.slice(1),
-            data: [routeData]
+            data: [routeData],
           };
         count++;
         }
-
+        if (!(timestamp in timesForTotal)) {
+          timesForTotal[timestamp] = totalCounter;
+          totalData.data[totalCounter++] = {
+            hits: 0,
+            time: timestamp,
+          };
+        }
+        totalData.data[timesForTotal[timestamp]].hits += routeDataPoint.statValue;
       });
+      graphData.unshift(totalData);
       res.status(200).send(graphData);
     })
     .catch(function(error) {
