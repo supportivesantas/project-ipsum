ca57ee7c0535c78eee707b652c3c950d999e0fef
Jonathan Mah <jonathan.mah.7@gmail.com>
2016-05-09 16:35:43 -0700

    auto discovery for load balancer.

--- server/controllers/nginxController.js
+++ server/controllers/nginxController.js
@@ -1,3 +1,4 @@
+<<<<<<< 5f294149d33e18fc403a90725744eed3f118a388
 const util = require('util');
 const exec = require('child_process').exec;
 const LoadBalancer = require('../db/models/loadbalancer.js');
@@ -5,6 +6,14 @@ const LoadBalancers = require('../db/collections/loadbalancers.js');
 const Server = require('../db/models/client-server.js');
 const Servers = require('../db/collections/client-server.js');
 const log = function(error, stdout, stderr) {
+=======
+"use strict";
+var sys = require('sys');
+var exec = require('child_process').exec;
+var requestP = require('request-promise');
+
+var log = function(error, stdout, stderr) {
+>>>>>>> auto discovery for load balancer.
   console.log(stdout);
 };
 const _ = require('underscore');
@@ -14,7 +23,32 @@ module.exports = {
   //NEED TO GET ID FOR REMOVE
 
   list(nginxipandport, zone) {
-    exec("curl http://" + nginxipandport + "/upstream_conf?upstream=" + zone + "&verbose=", log);
+    let options = {
+      uri: 'http://' + nginxipandport + '/upstream_conf',
+      qs: {
+        upstream: zone,
+        verbose: ''
+      }
+    };
+
+    return requestP(options)
+      .then((result) => {
+        let servers = [];
+        let parseServerInfo = /server ([0-9\.]+):([0-9]+)/g;
+        let serverInfo = null;
+        while (serverInfo = parseServerInfo.exec(result)) {
+          servers.push({
+            ip: serverInfo[1],
+            port: serverInfo[2]
+          });
+        }
+
+        return servers;
+      })
+      .catch((error) => {
+        console.log(error);
+        return;
+      });
   },
   enable(nginxipandport, targetipandport, zone) {
     exec("curl http://" + nginxipandport + "/upstream_conf?upstream=" + zone + "&server=" + targetipandport + "&up=", log);
