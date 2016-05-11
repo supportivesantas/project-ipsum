10679354542b3a089070375aaf740aa4079e77e7
Jonathan Mah <jonathan.mah.7@gmail.com>
2016-05-11 10:36:10 -0700

    removed lb_id

--- server/controllers/internal_tasks.js
+++ server/controllers/internal_tasks.js
@@ -99,7 +99,6 @@ internalTasks.syncServersToLB = function (userID, overwriteAll) {
       return Promise.all(servers.map((server) => {
         if (quickLook[server.get('ip')] && (!server.get('master') || overwriteAll)) {
           server.set('master', quickLook[server.get('ip')].lbID);
-          // server.set('lb_id', quickLook[server.get('ip')].id);
           return server.save();
         } else {
           return;
