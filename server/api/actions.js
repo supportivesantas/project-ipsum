7394dd3b51d7fd79e502671ed50cbf5bc6daaa17
Jonathan Mah <jonathan.mah.7@gmail.com>
2016-05-12 11:30:36 -0700

    partial implementation for spinning up servers and attaching to LB.

--- server/api/actions.js
+++ server/api/actions.js
@@ -2,7 +2,9 @@ var actions = [
   'list_all_servers',
   'list_all_images',      
   'delete_server',
-  'create_server',  
+  'get_server',                /* get server info for one server by server_id                       */
+  'get_server_pltfm_specific', /* get all platform specific info needed to create a server          */
+  'create_server',             /* creates new server with existing image id and pltfm specific info */
   'reboot_server',   
   'power_on_server', 
   'power_off_server',
